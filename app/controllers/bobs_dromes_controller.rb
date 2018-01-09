c359ab3e08bc5f89fd08f6164a1d0bdfdbc4cebe
Bob Habig <rfhab2@gmail.com>
2018-01-09 15:00:32 -0500

    form input stickiness done

--- app/controllers/bobs_dromes_controller.rb
+++ app/controllers/bobs_dromes_controller.rb
@@ -1,16 +1,19 @@
 class BobsDromesController < ApplicationController
   def home
+    if palindrome_params
+      @value = palindrome_params[:input]
+      flash[:notice] = BobsDrome.input_check(@value)
+      render :home and return
+    end
   end
 
-  def input_check
-    @value = palindrome_params[:input]
-    flash[:notice] = BobsDrome.input_check(@value)
-    redirect_to root_path
-  end
+  # def input_check
+  #
+  # end
 
   private
 
   def palindrome_params
-    params.require(:palindrome).permit(:input)
+    params.require(:palindrome).permit(:input) if params[:palindrome]
   end
 end
