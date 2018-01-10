d3d6138e4135b8a0d1eefb8a78573172ec443a39
Bob Habig <rfhab2@gmail.com>
2018-01-09 23:13:11 -0500

    redirect not render

--- app/controllers/bobs_dromes_controller.rb
+++ app/controllers/bobs_dromes_controller.rb
@@ -1,19 +1,19 @@
 class BobsDromesController < ApplicationController
   def home
-    if palindrome_params
-      @value = palindrome_params[:input]
-      flash[:notice] = BobsDrome.input_check(@value)
-      render :home and return
-    end
   end
 
-  # def input_check
-  #
-  # end
+  def input_check
+
+    @value = palindrome_params[:input]
+    flash[:notice] = BobsDrome.input_check(@value)
+    session[:value] = @value
+    redirect_to root_path
+
+  end
 
   private
 
   def palindrome_params
-    params.require(:palindrome).permit(:input) if params[:palindrome]
+    params.require(:palindrome).permit(:input)
   end
 end
